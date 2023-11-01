83eed4f3c371b0e8d0e66e7c12b4e2f3994f6e7a
Shabdan <tokmokiuca@gmail.com>
2023-11-01 12:53:11 +0600

    s

---
+++ script.js
@@ -0,0 +1,50 @@
+// let arr = [
+//     {
+//         name: 'Shabdan',
+//         age: 21,
+//         city: 'Tokmok',
+//         country: 'Kyrgyzstan',
+//         job: 'billioner',
+//         salary: 1200,
+//         married: true,
+//     },
+//     {
+//         name: 'Farukh',
+//         age: 22,
+//         city: 'Tokmok',
+//         country: 'Kyrgyzstan',
+//         job: 'cleaner',
+//         salary: 100,
+//         married: false,
+//     },
+//     {
+//         name: 'Beksultan',
+//         age: 30,
+//         city: 'Tokmok',
+//         country: 'Kyrgyzstan',
+//         job: 'billioner',
+//         salary: 3000,
+//         married: true,
+//     },
+//     {
+//         name: 'Shabdan',
+//         age:  23,
+//         city: 'Tokmok',
+//         country: 'Kyrgyzstan',
+//         job: 'billioner',
+//         salary: 2400,
+//         married: true,
+//     },
+// ]
+// // console.log(arr.filter(el => el.age > 23))
+// console.log(arr.map(el => el.job === 'billioner' ? {...el, salary:((el.salary / 100) * 25) + el.salary} : el))
+
+// let a = [1,2]
+// let b = [ 'Hello', 'World']
+// let c = [...a,...b.join('').split('')]
+// console.log(c)
+
+// let a = [1,5,3,6,8,9]
+// let b = ['b','c','d','s']
+// console.log(b.sort((a,b) => a < b ? -1: 1))
+// console.log(a.sort())
